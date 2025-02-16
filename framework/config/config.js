78df605e1f34cb3247d550ba86c43f03ba18f0e1
HooverX <freshvlad94@mail.ru>
2025-02-16 18:16:13 +0300

    y

--- framework/config/config.js
+++ framework/config/config.js
@@ -1,1 +1,9 @@
-export 
+import 'dotenv/config';
+
+export const config = {
+  baseURL: 'https://dummyjson.com',
+  credentials: {
+    username: process.env.TEST_DUMMY_JSON_USERNAME,
+    password: process.env.TEST_DUMMY_JSON_PASSWORD
+  }
+};
--- jest.config.js
+++ jest.config.js
@@ -109,7 +109,7 @@ const config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  reporters: [['github-actions', { silent: false }], 'summary']
+  reporters: [['github-actions', { silent: false }], 'summary'],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
@@ -135,7 +135,7 @@ const config = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+    setupFiles: ['dotenv/config']
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
